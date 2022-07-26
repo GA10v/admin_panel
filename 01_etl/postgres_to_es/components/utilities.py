2c33d8471af64dc1193dd6fcf943363035842149
Galovach Andrey <a.galovach@yandex.ru>
2022-07-26 16:57:31 +0300

    Добавил backoff

--- 01_etl/postgres_to_es/components/utilities.py
+++ 01_etl/postgres_to_es/components/utilities.py
@@ -1,32 +1,15 @@
 import logging
-import sqlite3
 from contextlib import contextmanager
-from pathlib import Path
 
 import psycopg2
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor
 
 from components import log_config, models_pg
+from functools import wraps
+import time
 
 log_config.get_log()
-
-
-@contextmanager
-def sqlite_conn_context(db_path: str) -> sqlite3.Connection:
-    """Контекстный менеджер для sqlite3.
-
-    Args:
-        db_path: Путь к файлу db.sqlite
-    """
-    try:
-        conn = sqlite3.connect(db_path)
-        conn.row_factory = sqlite3.Row
-        yield conn
-    except Exception as er:
-        logging.error(er)
-    finally:
-        conn.close()
 
 
 @contextmanager
@@ -36,6 +19,9 @@ def pg_conn_context(dsl: models_pg.DBConf, cursor_factory=DictCursor) -> _connec
     Args:
         dsl: Данные для подключения к psql
         cursor_factory: Курсор
+
+    Yields:
+        _connection: Конектор
     """
     try:
         conn = psycopg2.connect(**dsl, cursor_factory=cursor_factory)
@@ -47,44 +33,38 @@ def pg_conn_context(dsl: models_pg.DBConf, cursor_factory=DictCursor) -> _connec
         conn.close()
 
 
-def check_pg_models(dsl: models_pg.DBConf, ddl_path: str = f'{Path(__file__).parent.parent}/new_movies_database.ddl') -> bool:
-    """Функция проверки наличия схемы БД для 3-го модуля.
+def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
+    """
+    Функция для повторного выполнения функции через некоторое время, если возникла ошибка. Использует наивный экспоненциальный рост времени повтора (factor) до граничного времени ожидания (border_sleep_time)
+
+    Формула:
+        t = start_sleep_time * 2^(n) if t < border_sleep_time
+        t = border_sleep_time if t >= border_sleep_time
 
     Args:
-        dsl: Данные для подключения к pg
-        ddl_path: Путь к файлу с настройками БД
+        start_sleep_time: Начальное время повтора
+        factor: Во сколько раз нужно увеличить время ожидания
+        border_sleep_time: Граничное время ожидания
+
+    Returns: 
+        func_wrapper: Результат выполнения функции
     """
-    with pg_conn_context(dsl) as pg_conn:
-        with pg_conn.cursor() as cursor:
-            table_names = {
-                'film_work',
-                'person',
-                'genre',
-                'person_film_work',
-                'genre_film_work',
-            }
-            try:
-                cursor.execute("SELECT table_name \
-                                FROM information_schema.tables \
-                                WHERE table_schema = 'content';")
-                                
-                if not table_names.issubset(set([colum[0] for colum in cursor])):
-
-                    logging.warning('Схема content не соответствует db.sqlite')
-                    logging.info('Запуск миграции данных из db.sqlite')
-
-                    with open(ddl_path, 'r') as f:
-                        cursor.execute(f.read())
-                        pg_conn.commit()
-                    return True
-
-                cursor.execute("SELECT COUNT(id) FROM content.film_work;")                                
-                if [_[0] for _ in cursor][0] < 999 :
-                    logging.warning('Количество записей в "movies_database" не соответствует db.sqlite')
-                    logging.info('Запуск миграции данных из db.sqlite')
-                    return True
-                else:
-                    logging.info('БД "movies_database" соответствует db.sqlite')
-                    return False
-            except (FileNotFoundError, psycopg2.OperationalError) as er:
-                logging.error(er)
+    def func_wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            sleep_time = start_sleep_time
+            n = 1
+            while True:
+                try:
+                    return func(*args, **kwargs)
+                # разобраться с исключениями
+                except Exception as er:
+                    logging.error(er)
+                    time.sleep(sleep_time)
+                    if sleep_time >= border_sleep_time:
+                        sleep_time = border_sleep_time
+                    else:
+                        sleep_time = start_sleep_time * 2 ^ (n)
+                        n *= factor
+        return inner
+    return func_wrapper
