1c7304489669760050606dc9395c4bdbae681da6
Galovach Andrey <a.galovach@yandex.ru>
2022-07-27 20:08:57 +0300

    Добавил sql query в constants.py

--- 01_etl/postgres_to_es/components/constants.py
+++ 01_etl/postgres_to_es/components/constants.py
@@ -6,8 +6,7 @@ from . import models_pg
 
 load_dotenv()
 
-# добавить pg
-DSL = {
+DSL_PG = {
     'host': os.environ.get('DB_HOST'),
     'database': os.environ.get('DB_NAME'),
     'user': os.environ.get('DB_USER'),
@@ -15,6 +14,37 @@ DSL = {
     'port': os.environ.get('DB_PORT'),
 }
 
+
+SQL_QUERY = """
+    SELECT
+    fw.id,
+    fw.title,
+    fw.description,
+    fw.rating,
+    fw.type,
+    fw.created,
+    fw.modified,
+    COALESCE (
+        json_agg(
+            DISTINCT jsonb_build_object(
+                'person_role', pfw.role,
+                'person_id', p.id,
+                'person_name', p.full_name
+            )
+        ) FILTER (WHERE p.id is not null),
+        '[]'
+    ) as persons,
+    array_agg(DISTINCT g.name) as genres
+    FROM content.film_work fw
+    LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
+    LEFT JOIN content.person p ON p.id = pfw.person_id
+    LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
+    LEFT JOIN content.genre g ON g.id = gfw.genre_id
+    WHERE fw.modified > '%s'
+    GROUP BY fw.id
+    ORDER BY fw.modified
+    LIMIT 100; 
+"""
 
 PG_MODELS = {
     'film_work': models_pg.FilmWorkPG,
