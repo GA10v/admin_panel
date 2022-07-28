3302a290772c1ff9cd00650975892103f2814cc2
Galovach Andrey <a.galovach@yandex.ru>
2022-07-28 21:38:10 +0300

    Исправил логику PostgresExtractor

---
+++ 01_etl/postgres_to_es/components/sql_queries.py
@@ -0,0 +1,62 @@
+from . import constants
+from typing import Generator
+import datetime
+
+def get_query(last_update_time: datetime) -> Generator[str, None, None]:
+    for table in constants.PG_MODELS:
+        query = f"""SELECT
+                    content.film_work.id
+                    FROM content.film_work 
+                    LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = content.film_work.id
+                    LEFT JOIN content.person ON content.person.id = pfw.person_id
+                    LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = content.film_work.id
+                    LEFT JOIN content.genre ON content.genre.id = gfw.genre_id
+                    WHERE content.{table}.modified < '{last_update_time}'
+                    GROUP BY content.film_work.id;"""
+        yield query
+
+
+SQL_QUERY = """
+    SELECT
+    fw.id,
+    fw.title,
+    fw.description,
+    fw.rating AS imdb_rating,
+    fw.type,
+    to_char(fw.modified, 'YYYY-MM-DD HH24:MI:SS.FF6TZH') AS modified,
+    COALESCE (
+            string_agg(DISTINCT p.full_name, '')
+            FILTER(WHERE p.id is not null AND pfw.role = 'director'),
+            ''
+            ) AS director,
+    array_agg(DISTINCT p.full_name) FILTER(WHERE p.id is not null AND pfw.role = 'actor') AS actors_names,
+    array_agg(DISTINCT p.full_name) FILTER(WHERE p.id is not null AND pfw.role = 'writer') AS writers_names,
+    COALESCE (
+            json_agg(
+                DISTINCT jsonb_build_object(
+                    'id', p.id,
+                    'name', p.full_name
+                )
+            ) FILTER (WHERE p.id is not null AND pfw.role = 'actor'),
+            '[]'
+        ) as actors,
+    COALESCE (
+            json_agg(
+                DISTINCT jsonb_build_object(
+                    'id', p.id,
+                    'name', p.full_name
+                )
+            ) FILTER (WHERE p.id is not null AND pfw.role = 'writer'),
+            '[]'
+        ) as writers,
+    array_agg(DISTINCT g.name) as genres
+    FROM content.film_work fw
+    LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
+    LEFT JOIN content.person p ON p.id = pfw.person_id
+    LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
+    LEFT JOIN content.genre g ON g.id = gfw.genre_id
+    WHERE fw.id IN (%s)
+    GROUP BY fw.id
+    ORDER BY modified
+    LIMIT 100;
+"""
--- 01_etl/postgres_to_es/pg_extractor.py
+++ 01_etl/postgres_to_es/pg_extractor.py
@@ -1,8 +1,9 @@
 import datetime
 from psycopg2.extensions import connection as _connection
-from components import log_config, constants, utilities
+from components import log_config, constants, utilities, sql_queries
 import logging
 from typing import Generator
+import json
 
 log_config.get_log()
 
@@ -12,20 +13,44 @@ class PostgresExtractor:
         self.connection = connection
         self.batch_size = batch_size
 
-    def get_data(self, last_update_time: datetime, query: str) -> Generator[list, None, None]:
+    def get_id(self, last_update_time: datetime) -> set:
+        """
+        Получение из PostgreSQL всех id для таблицы filmwork в которых были внесены изменения.
+
+        Args:
+            last_update_time: Время последнего обновления данных.
+
+        Returns:
+            result: Множество всех id для таблицы filmwork в которых были внесены изменения.
+        """
+        
+        ids = []
+        try:
+            for query in sql_queries.get_query(last_update_time):
+                with self.connection.cursor() as cursor:
+                    cursor.execute(query)
+                    pg_data=cursor.fetchall()
+                    ids.append((str(x[0]) for x in pg_data))
+            result = set(ids[0])
+            return result
+        except Exception as er:
+            logging.error(er)
+
+    def get_data(self, last_update_time: datetime) -> Generator[list, None, None]:
         """
         Получение данных из PostgreSQL.
 
         Args:
             last_update_time: Время последнего обновления данных
-            query: Запрос к PostgreSQL
 
         Yields:
             Generator: Список словарей с данными из PostgreSQL
         """
         try:
+            ids = [id for id in self.get_id(last_update_time)]
+            ids_count = ', '.join('%s' for _ in range(len(ids)))
             with self.connection.cursor() as cursor:
-                cursor.execute(constants.SQL_QUERY % last_update_time)
+                cursor.execute(sql_queries.SQL_QUERY % ids_count, ids)
 
                 desc = cursor.description
                 column_names = [col[0] for col in desc]
@@ -38,6 +63,7 @@ class PostgresExtractor:
 
 if __name__ == '__main__':
     with utilities.pg_conn_context(constants.DSL_PG) as pg_conn:
-        pg_loader = PostgresExtractor(connection=pg_conn, batch_size=1)
-        data = pg_loader.get_data(last_update_time=datetime.datetime.now())
-        print([x[0] for x in data])
+        pg_loader=PostgresExtractor(connection = pg_conn, batch_size = 100)
+        data=pg_loader.get_data(last_update_time = datetime.datetime.now())
+        with open('2.json', 'w') as f:
+            json.dump([x for x in data][0], f, indent=4)
