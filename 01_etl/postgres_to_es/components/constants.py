b82a442d0c04a8a951ea260053a2be8d08f48f7a
Galovach Andrey <a.galovach@yandex.ru>
2022-07-28 17:10:43 +0300

    Добавил запоросы к pg, изменил PostgresExtractor

--- 01_etl/postgres_to_es/components/constants.py
+++ 01_etl/postgres_to_es/components/constants.py
@@ -2,9 +2,10 @@ import os
 
 from dotenv import load_dotenv
 
-from . import models_pg
 
 load_dotenv()
+
+PG_MODELS = ['filmwork', 'person', 'genre']
 
 DSL_PG = {
     'host': os.environ.get('DB_HOST'),
@@ -14,42 +15,143 @@ DSL_PG = {
     'port': os.environ.get('DB_PORT'),
 }
 
-
-SQL_QUERY = """
+SQL_QUERY_FW = """
     SELECT
     fw.id,
     fw.title,
     fw.description,
-    fw.rating,
+    fw.rating AS imdb_rating,
     fw.type,
-    fw.created,
-    fw.modified,
+    to_char(fw.modified, 'YYYY-MM-DD HH24:MI:SS.FF6TZH') AS modified,
     COALESCE (
-        json_agg(
-            DISTINCT jsonb_build_object(
-                'person_role', pfw.role,
-                'person_id', p.id,
-                'person_name', p.full_name
-            )
-        ) FILTER (WHERE p.id is not null),
-        '[]'
-    ) as persons,
+            string_agg(DISTINCT p.full_name, '')
+            FILTER(WHERE p.id is not null AND pfw.role = 'director'),
+            ''
+            ) AS director,
+    array_agg(DISTINCT p.full_name) FILTER(WHERE p.id is not null AND pfw.role = 'actor') AS actors_names,
+    array_agg(DISTINCT p.full_name) FILTER(WHERE p.id is not null AND pfw.role = 'writer') AS writers_names,
+    COALESCE (
+            json_agg(
+                DISTINCT jsonb_build_object(
+                    'id', p.id,
+                    'name', p.full_name
+                )
+            ) FILTER (WHERE p.id is not null AND pfw.role = 'actor'),
+            '[]'
+        ) as actors,
+    COALESCE (
+            json_agg(
+                DISTINCT jsonb_build_object(
+                    'id', p.id,
+                    'name', p.full_name
+                )
+            ) FILTER (WHERE p.id is not null AND pfw.role = 'writer'),
+            '[]'
+        ) as writers,
     array_agg(DISTINCT g.name) as genres
     FROM content.film_work fw
     LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
     LEFT JOIN content.person p ON p.id = pfw.person_id
     LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
     LEFT JOIN content.genre g ON g.id = gfw.genre_id
-    WHERE fw.modified > '%s'
+    WHERE fw.modified < '%s'
     GROUP BY fw.id
-    ORDER BY fw.modified
-    LIMIT 100; 
+    ORDER BY modified
+    LIMIT 100;
 """
 
-PG_MODELS = {
-    'film_work': models_pg.FilmWorkPG,
-    'genre': models_pg.GenrePG,
-    'genre_film_work': models_pg.GenreFilmWorkPG,
-    'person': models_pg.PersonPG,
-    'person_film_work': models_pg.PersonFilmWorkPG,
+SQL_QUERY_P = """
+        SELECT
+    fw.id,
+    fw.title,
+    fw.description,
+    fw.rating AS imdb_rating,
+    fw.type,
+    to_char(p.modified, 'YYYY-MM-DD HH24:MI:SS.FF6TZH') AS modified,
+    COALESCE (
+            string_agg(DISTINCT p.full_name, '')
+            FILTER(WHERE p.id is not null AND pfw.role = 'director'),
+            ''
+            ) AS director,
+    array_agg(DISTINCT p.full_name) FILTER(WHERE p.id is not null AND pfw.role = 'actor') AS actors_names,
+    array_agg(DISTINCT p.full_name) FILTER(WHERE p.id is not null AND pfw.role = 'writer') AS writers_names,
+    COALESCE (
+            json_agg(
+                DISTINCT jsonb_build_object(
+                    'id', p.id,
+                    'name', p.full_name
+                )
+            ) FILTER (WHERE p.id is not null AND pfw.role = 'actor'),
+            '[]'
+        ) as actors,
+    COALESCE (
+            json_agg(
+                DISTINCT jsonb_build_object(
+                    'id', p.id,
+                    'name', p.full_name
+                )
+            ) FILTER (WHERE p.id is not null AND pfw.role = 'writer'),
+            '[]'
+        ) as writers,
+    array_agg(DISTINCT g.name) as genres
+    FROM content.film_work fw
+    LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
+    LEFT JOIN content.person p ON p.id = pfw.person_id
+    LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
+    LEFT JOIN content.genre g ON g.id = gfw.genre_id
+    WHERE p.modified < '%s'
+    GROUP BY fw.id
+    ORDER BY modified
+    LIMIT 100;
+"""
+
+SQL_QUERY_G = """
+        SELECT
+    fw.id,
+    fw.title,
+    fw.description,
+    fw.rating AS imdb_rating,
+    fw.type,
+    to_char(g.modified, 'YYYY-MM-DD HH24:MI:SS.FF6TZH') AS modified,
+    COALESCE (
+            string_agg(DISTINCT p.full_name, '')
+            FILTER(WHERE p.id is not null AND pfw.role = 'director'),
+            ''
+            ) AS director,
+    array_agg(DISTINCT p.full_name) FILTER(WHERE p.id is not null AND pfw.role = 'actor') AS actors_names,
+    array_agg(DISTINCT p.full_name) FILTER(WHERE p.id is not null AND pfw.role = 'writer') AS writers_names,
+    COALESCE (
+            json_agg(
+                DISTINCT jsonb_build_object(
+                    'id', p.id,
+                    'name', p.full_name
+                )
+            ) FILTER (WHERE p.id is not null AND pfw.role = 'actor'),
+            '[]'
+        ) as actors,
+    COALESCE (
+            json_agg(
+                DISTINCT jsonb_build_object(
+                    'id', p.id,
+                    'name', p.full_name
+                )
+            ) FILTER (WHERE p.id is not null AND pfw.role = 'writer'),
+            '[]'
+        ) as writers,
+    array_agg(DISTINCT g.name) as genres
+    FROM content.film_work fw
+    LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
+    LEFT JOIN content.person p ON p.id = pfw.person_id
+    LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
+    LEFT JOIN content.genre g ON g.id = gfw.genre_id
+    WHERE g.modified < '%s'
+    GROUP BY fw.id
+    ORDER BY modified
+    LIMIT 100;
+"""
+
+SQL_QUERIES = {
+    'filmwork': SQL_QUERY_FW,
+    'person': SQL_QUERY_P,
+    'genre': SQL_QUERY_G,
 }
