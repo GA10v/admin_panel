965a403e84b24a6131adf04f5d371e6a5dbcd07b
Galovach Andrey <a.galovach@yandex.ru>
2022-08-01 22:02:58 +0300

    Добавил backoff в ESLoader

--- 01_etl/postgres_to_es/elastic_loader.py
+++ 01_etl/postgres_to_es/elastic_loader.py
@@ -1,26 +1,38 @@
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk as _bulk
 import logging
-from components import log_config, state, constants, schema, utilities, models
+from components import log_config, state, constants, schema, models
 from pg_extractor import PostgresExtractor
 from data_transform import DataTransformer
 from components.state import State
+from components.utilities import backoff
 
 log_config.get_log()
 
 
 class ESLoader:
-    def __init__(self, connection: Elasticsearch, index_name: str, state_maneger: State):
+    def __init__(self, dsl: str, index_name: str, state_maneger: State):
         """
         Args:
-            connection: Клиент для связи с сервером Elasticsearch.
+            dsl: Клиент для связи с сервером Elasticsearch.
             index_name: Индекс.
             state_maneger: Объект класса State для хранения состояний.
         """
-        self.connection = connection
+        self.dsl = dsl
         self.index_name = index_name
         self.state_maneger = state_maneger
+        self.connection = self.get_connection()
 
+    @backoff()
+    def get_connection(self) -> Elasticsearch:
+        """ Реализация отказоустойчивости.
+
+        Returns:
+            Elasticsearch: Объект класса Elasticsearch, конектор.      
+        """
+        return Elasticsearch(self.dsl)
+
+    @backoff()
     def check_connection(self) -> None:
         """Проверка связи с сервером Elasticsearch."""
 
@@ -37,6 +49,7 @@ class ESLoader:
             self.connection.indices.create(index=self.index_name, body=body)
         except Exception as er:
             logging.error(er)
+            # raise er
 
     def push_bulk(self, bulk: list[models.ESDataConf], last_update_time: str) -> None:
         """
@@ -50,11 +63,13 @@ class ESLoader:
         try:
             self.check_connection()
             _bulk(self.connection, actions=bulk, index=self.index_name)
+            logging.info('Данные успешно обновлены')
             self.state_maneger.set_state(
                 key=constants.STATE_KEY, value=last_update_time)
+            logging.info(f'Дата последнего обновления данных: {last_update_time}')
         except Exception as er:
             logging.error(er)
-            raise er
+            # raise er
 
 
 if __name__ == '__main__':
@@ -62,18 +77,18 @@ if __name__ == '__main__':
     state_maneger = state.State(storage)
     es_conn = Elasticsearch(constants.ES_URL)
     transformer = DataTransformer()
-    loader = ESLoader(es_conn, constants.ES_INDEX, state_maneger)
-    
+    loader = ESLoader(constants.ES_URL, constants.ES_INDEX, state_maneger)
+    es_conn = loader.connection
+
     if not es_conn.indices.exists(index=constants.ES_INDEX):
         loader.push_index()
 
-    with utilities.pg_conn_context(constants.DSL_PG) as pg_conn:
-        extractor = PostgresExtractor(connection=pg_conn, state_maneger=state_maneger, batch_size=100)
-        pg_data = [x for x in extractor.get_data()]
-        if pg_data:
-            bulk = transformer.compile_data(pg_data=pg_data)
-            print(bulk)
-            last_update_time = transformer.last_update_time
-            loader.push_bulk(bulk=bulk, last_update_time=last_update_time)
-        else:
-            print('no data')
+    extractor = PostgresExtractor(dsl=constants.DSL_PG, state_maneger=state_maneger, batch_size=100)
+    pg_data = [x for x in extractor.get_data()]
+    if pg_data:
+        bulk = transformer.compile_data(pg_data=pg_data)
+        last_update_time = transformer.last_update_time
+        print(last_update_time)
+        loader.push_bulk(bulk=bulk, last_update_time=last_update_time)
+    else:
+        print('no data')
